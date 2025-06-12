431734076fce32c6968c97b995a2e3b1f2756dbf
ashley sanchez <130068110+ashaps03@users.noreply.github.com>
2025-06-12 11:56:52 +0200

    feat: added a router and home/dashboard pages.

--- job-tracker-frontend/src/App.jsx
+++ job-tracker-frontend/src/App.jsx
@@ -1,53 +1,16 @@
-import { useState, useEffect } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import axios from 'axios';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Dashboard from './pages/Dashboard';
+import Navigation from './components/Navigation';
 
-function App() {
-  const [count, setCount] = useState(0)
-  const [array, setArray] = useState([]);
-
-
-  const fetchAPI = async () => {
-    const response = await axios.get("http://127.0.0.1:8080/api/users")
-    console.log(response.data.users);
-    setArray(response.data.users);
-};
-useEffect(() => {
-  fetchAPI()
-}, [])
-
+export default function App() {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <h1>TESTING</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-          {
-            array.map((user, index) => (
-              <div key={index}>
-                <span>{user}</span>
-                <br></br>
-              </div> 
-            ))
-          }
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <Router>
+      <Navigation />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+      </Routes>
+    </Router>
+  );
 }
-
-export default App
